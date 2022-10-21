a0a6be629d0df26e8d027c636e3fbca6ecf799a0
Scott Gebert <scott22.gebert@gmail.com>
2022-10-21 13:00:18 -0600

    DBfunctions update

--- src/MiniProjectOne.py
+++ src/MiniProjectOne.py
@@ -1,7 +1,26 @@
+import sqlite3
+import time
+import dbFunctions
 import login
+import testQuery
+
+
+path = "miniProject.db"
 
 def main():
+    # Initalizes Connection to the DB - use dbFunctions.Cursor for executing queries  
+    dbFunctions.connect(path)
+
     id, userType = login.getLoginInfo()
+    
+    testQuery.run(dbFunctions.cursor, dbFunctions.connection)
+
+
+
+    dbFunctions.connection.commit()
+    dbFunctions.connection.close()
+    return
+
 
 if __name__ == "__main__":
     main()
--- src/dbFunctions.py
+++ src/dbFunctions.py
@@ -4,14 +4,15 @@ import time
 connection = None
 cursor = None
 
+
 def connect(path):
     global connection, cursor
-
     connection = sqlite3.connect(path)
     cursor = connection.cursor()
     cursor.execute(' PRAGMA foreign_keys=ON; ')
     connection.commit()
-    return
+    return cursor
+
 
 
 def createTables():
